c380dcbbc1610d836adb0ebae9427d61b69f12f0
tjacquecin <93091146+tjacquecin@users.noreply.github.com>
2021-12-29 18:35:34 -0500

    Added days

--- src/index.js
+++ src/index.js
@@ -13,6 +13,33 @@ let hour = now.getHours();
 let minute = now.getMinutes();
 Date.innerHTML = `${day} ${hour}:${minute}`;
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `  <div class="col-2"> 
+                <div class="weather-forecast-date">${day}</div>
+                <img
+                src="http://openweathermap.org/img/wn/10d@2x.png"
+                alt=""
+                width="42"
+                />
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-max"> 
+                    18° </span>
+                  <span class="weather-forecast-temperature-min"> 
+                    12° </span>
+                </div>
+              </div>
+              `;
+  });
+  let forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayWeatherCondition(response) {
   console.log(response.data);
   document.querySelector("#city").innerHTML = response.data.name;
@@ -70,3 +97,4 @@ let showLocationButton = document.querySelector("#show-location-button");
 showLocationButton.addEventListener("click", showLocation);
 
 searchCity("Boston");
+displayForecast();
