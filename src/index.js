0973e97eb29d744a90ae9a77cc6c74b42d32dffc
tjacquecin <93091146+tjacquecin@users.noreply.github.com>
2021-12-29 18:42:47 -0500

    more changes

--- src/index.js
+++ src/index.js
@@ -7,26 +7,27 @@ Date.innerHTML = `${day} ${hour}:${minute}`;
 
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
   let forecastHTML = `<div class="row">`;
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
-      `  <div class="col-2"> 
-                <div class="weather-forecast-date">${day}</div>
-                <img
-                src="http://openweathermap.org/img/wn/10d@2x.png"
-                alt=""
-                width="42"
-                />
-                <div class="weather-forecast-temperatures">
-                  <span class="weather-forecast-temperature-max"> 
-                    18° </span>
-                  <span class="weather-forecast-temperature-min"> 
-                    12° </span>
-                </div>
-              </div>
-              `;
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
   });
   let forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
